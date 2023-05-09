b7afeaa672dc3d11f2b1ab648060a58b60fe2624
SROTH <simon.roth@ynov.com>
2023-05-09 10:31:19 +0200

    init project

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,11 +1,9 @@
 package org.example;
 
 
-import java.io.IOException;
-
 public class App
 {
-    public static void main( String[] args ) throws IOException {
-        Extract.ExtractProduits();
+    public static void main( String[] args ){
+
     }
 }
